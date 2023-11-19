a02d45a0617309d34a695384fadb2e6af5ac3746
Ishaan <116852003+ikap22@users.noreply.github.com>
2023-11-19 13:06:37 -0800

    Update FinanceManager.java

--- FinanceManager.java
+++ FinanceManager.java
@@ -2,10 +2,6 @@ public class FinanceManager {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
 
-
-
-      
-        
         userInterface.start(); 
     }
 }
